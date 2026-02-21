32e838da178aa86d485e278e52b8fa6c9d1653ae
Lisha from Black-Windows10-HP <lishastudies.cs@gmail.com>
2026-02-21 23:30:16 +0530

    fixed std::array inconsistencies

--- src/Blocks.cpp
+++ src/Blocks.cpp
@@ -1,6 +1,7 @@
 #include "Blocks.h"
 #include "Colors.h"
 #include "Position.h"
+#include <array>
 
 Block::Block() {
    cellSize = 30;
@@ -8,9 +9,10 @@ Block::Block() {
 }
 
 void Block::Draw() {
-   std::array<Position, 4> block = this->cells[rotation];
+   std::array<Position, 4> block = this->cells.at(rotation);
 
-   for(const Position& cell : block) {
+   for(int i = 0; i < 4; i++) {
+      Position& cell = block.at(i);
       DrawRectangle(
          cell.col * cellSize + 1, cell.row * cellSize + 1,
          cellSize - 1, cellSize - 1, cellColorMap[id]
@@ -22,56 +24,53 @@ void Block::Draw() {
 
 Hero::Hero() {
    id = CellType::Hero;
-   cells.at(RotationState::Zero) = {{ {1, 0}, {1, 1}, {1, 2}, {1, 3} }};
-   cells.at(RotationState::Ninety) = {{ {0, 2}, {1, 2}, {2, 2}, {3, 2} }};
-   cells.at(RotationState::OneEighty) = {{ {2, 0}, {2, 1}, {2, 2}, {2, 3} }};
-   cells.at(RotationState::TwoSeventy) = {{ {0, 1}, {1, 1}, {2, 1}, {3, 1} }};
+   cells[RotationState::Zero] = {{ {1, 0}, {1, 1}, {1, 2}, {1, 3} }};
+   cells[RotationState::Ninety] = {{ {0, 2}, {1, 2}, {2, 2}, {3, 2} }};
+   cells[RotationState::OneEighty] = {{ {2, 0}, {2, 1}, {2, 2}, {2, 3} }};
+   cells[RotationState::TwoSeventy] = {{ {0, 1}, {1, 1}, {2, 1}, {3, 1} }};
 }
 
 Teewee::Teewee() {
    id = CellType::Teewee;
-   cells.at(RotationState::Zero) = {{ {1, 0}, {1, 1}, {1, 2}, {0, 1} }};
-   cells.at(RotationState::Ninety) = {{ {0, 1}, {1, 1}, {2, 1}, {1, 2} }};
-   cells.at(RotationState::OneEighty) = {{ {1, 0}, {1, 1}, {1, 2}, {2, 1} }};
-   cells.at(RotationState::TwoSeventy) = {{ {0, 1}, {1, 1}, {2, 1}, {1, 0} }};
+   cells[RotationState::Zero] = {{ {1, 0}, {1, 1}, {1, 2}, {0, 1} }};
+   cells[RotationState::Ninety] = {{ {0, 1}, {1, 1}, {2, 1}, {1, 2} }};
+   cells[RotationState::OneEighty] = {{ {1, 0}, {1, 1}, {1, 2}, {2, 1} }};
+   cells[RotationState::TwoSeventy] = {{ {0, 1}, {1, 1}, {2, 1}, {1, 0} }};
 }
 
 Smashboy::Smashboy() {
    id == CellType::Smashboy;
-   cells.at(RotationState::Zero) = {{ {0, 0}, {0, 1}, {1, 0}, {1, 1} }};
-   cells.at(RotationState::Ninety) = {{ {0, 0}, {0, 1}, {1, 0}, {1, 1} }};
-   cells.at(RotationState::OneEighty) = {{ {0, 0}, {0, 1}, {1, 0}, {1, 1} }};
-   cells.at(RotationState::TwoSeventy) = {{ {0, 0}, {0, 1}, {1, 0}, {1, 1} }};
+   cells[RotationState::Zero] = {{ {0, 0}, {0, 1}, {1, 0}, {1, 1} }};
 }
 
 Ricky::Ricky() {
    id = CellType::Ricky;
-   cells.at(RotationState::Zero) = {{ {0, 2}, {1, 2}, {1, 1}, {1, 0}} }; 
-   cells.at(RotationState::Ninety) = {{ {2, 2}, {2, 1}, {1, 1}, {0, 1} }};
-   cells.at(RotationState::OneEighty) = {{ {2, 0}, {1, 0}, {1, 1}, {1, 2} }};
-   cells.at(RotationState::TwoSeventy) = {{ {0, 0}, {0, 1}, {1, 1}, {2, 1} }};
+   cells[RotationState::Zero] = {{ {0, 2}, {1, 2}, {1, 1}, {1, 0}} }; 
+   cells[RotationState::Ninety] = {{ {2, 2}, {2, 1}, {1, 1}, {0, 1} }};
+   cells[RotationState::OneEighty] = {{ {2, 0}, {1, 0}, {1, 1}, {1, 2} }};
+   cells[RotationState::TwoSeventy] = {{ {0, 0}, {0, 1}, {1, 1}, {2, 1} }};
 }
 
 AutRicky::AutRicky() {
    id = CellType::AutRicky;
-   cells.at(RotationState::Zero) = {{ {0, 0}, {1, 0}, {1, 1}, {1, 2} }};
-   cells.at(RotationState::Ninety) = {{ {0, 2}, {0, 1}, {1, 1}, {2, 1} }};
-   cells.at(RotationState::OneEighty) = {{ {2, 2}, {1, 2}, {1, 1}, {1, 0} }};
-   cells.at(RotationState::TwoSeventy) = {{ {2, 0}, {2, 1}, {1, 1}, {0, 1} }};
+   cells[RotationState::Zero] = {{ {0, 0}, {1, 0}, {1, 1}, {1, 2} }};
+   cells[RotationState::Ninety] = {{ {0, 2}, {0, 1}, {1, 1}, {2, 1} }};
+   cells[RotationState::OneEighty] = {{ {2, 2}, {1, 2}, {1, 1}, {1, 0} }};
+   cells[RotationState::TwoSeventy] = {{ {2, 0}, {2, 1}, {1, 1}, {0, 1} }};
 }
 
 Snake::Snake() {
    id = CellType::Snake;
-   cells.at(RotationState::Zero) = {{ {1, 0}, {1, 1}, {0, 1}, {0, 2} }};
-   cells.at(RotationState::Ninety) = {{ {0, 1}, {1, 1}, {1, 2}, {2, 2} }};
-   cells.at(RotationState::OneEighty) = {{ {2, 0}, {2, 1}, {1, 1}, {1, 2} }};
-   cells.at(RotationState::TwoSeventy) = {{ {0, 0}, {1, 0}, {1, 1}, {2, 1} }};
+   cells[RotationState::Zero] = {{ {1, 0}, {1, 1}, {0, 1}, {0, 2} }};
+   cells[RotationState::Ninety] = {{ {0, 1}, {1, 1}, {1, 2}, {2, 2} }};
+   cells[RotationState::OneEighty] = {{ {2, 0}, {2, 1}, {1, 1}, {1, 2} }};
+   cells[RotationState::TwoSeventy] = {{ {0, 0}, {1, 0}, {1, 1}, {2, 1} }};
 }
 
 Znake::Znake() {
    id = CellType::Znake;
-   cells.at(RotationState::Zero) = {{ {0, 0}, {0, 1}, {1, 1}, {1, 2} }};
-   cells.at(RotationState::Ninety) = {{ {0, 2}, {1, 2}, {1, 1}, {2, 1} }};
-   cells.at(RotationState::OneEighty) = {{ {1, 0}, {1, 1}, {2, 1}, {2, 2} }};
-   cells.at(RotationState::TwoSeventy) = {{ {0, 1}, {1, 1}, {1, 0}, {2, 0} }};
+   cells[RotationState::Zero] = {{ {0, 0}, {0, 1}, {1, 1}, {1, 2} }};
+   cells[RotationState::Ninety] = {{ {0, 2}, {1, 2}, {1, 1}, {2, 1} }};
+   cells[RotationState::OneEighty] = {{ {1, 0}, {1, 1}, {2, 1}, {2, 2} }};
+   cells[RotationState::TwoSeventy] = {{ {0, 1}, {1, 1}, {1, 0}, {2, 0} }};
 }
